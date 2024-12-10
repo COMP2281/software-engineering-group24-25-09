1beba8a6d2599075fd49e0c3738b1ce0ee278674
fn <64966099+f2reninj5@users.noreply.github.com>
2024-12-10 15:20:04 +0000

    Added Ollama startup logic

--- llm/__init__.py
+++ llm/__init__.py
@@ -1,11 +1,28 @@
+import subprocess
+
 import ollama
 
 
 class LLM:
-    def start_ollama(self):
-        pass
+    def ollama_running(self):
+        try:
+            self.client.list()
+            return True
+        except:
+            return False
 
-    def __init__(self, url: str, model_name):
+    def _start_ollama(self):
+        subprocess.Popen(["ollama", "show", self.model_name]).wait()
+
+    def start_ollama(self):
+        if self.ollama_running():
+            print("Ollama is already running")
+            return
+        else:
+            self._start_ollama()
+
+    def __init__(self, url: str, model_name: str):
         self.url = url
         self.model_name = model_name
+        self.client = ollama.Client(host=self.url)
         self.start_ollama()

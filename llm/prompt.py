a3f74817a5335f584422f3886037c30b40b08e3b
fn <64966099+f2reninj5@users.noreply.github.com>
2024-12-10 20:29:34 +0000

    Updated prompt

--- llm/prompt.py
+++ llm/prompt.py
@@ -6,15 +6,15 @@ class Prompt:
     @staticmethod
     def summarise(source):
         return Prompt(
-            """You are summarising an article for a slide in a slideshow.
+            """You are generating bullet points for a presentation slide about IBM educational outreach.
             You will receive the content from a web page in the following format:
             Line 1: The URL of the web page
             Line 2: A sequence of lists representing images by their alt text and URL
             Line 3: A sequence of lists containing headings, where the lists are in order of importance
             Remaining lines: All text extracted from paragraph tags
-            Please summarise the article with a list of short sentences.
+            Please summarise the article with a list of very short sentences.
             Write each sentence on a new line.
-            Do not write anything else.
+            Do not confirm this message or say anything else.
             Avoid any formatting or indentation.""",
             source,
         )

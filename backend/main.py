4502eae1729b5de43f017e61541adb7bf7daca96
fn <64966099+f2reninj5@users.noreply.github.com>
2025-02-12 20:51:23 +0000

    Test engagements module in main

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,9 @@
 import os
 from backend.data.urls import urls
+from backend.engagements.llm import LLM
 from engagements import EngagementManager
 from dotenv import load_dotenv
 from urllib.parse import urlunparse
-from backend.engagements.llm import LLM
 
 
 def URL(scheme: str, netloc: str, url="", path="", query="", fragment=""):
@@ -21,14 +21,15 @@ def get_env():
 
 
 if __name__ == "__main__":
-    engagement_manager = EngagementManager("./data")
-    for url in urls:
-        engagement_manager.page_manager.add_page(url)
-    print(len(engagement_manager.page_manager.pages), "pages loaded")
-
     ollama_url, ollama_model_name = get_env()
     llm = LLM(ollama_url, ollama_model_name)
-    page = engagement_manager.page_manager.get_page(
-        "https://www.imperial.ac.uk/news/255517/phase-collaboration-sustainable-futures-between-imperial/"
-    )
-    print(llm.employees(page))
+    engagement_manager = EngagementManager(llm, "./data")
+
+    print(engagement_manager.get_engagements())
+    for url in urls:
+        engagement = engagement_manager.create_engagement_from_url(url)
+
+    slugs = engagement_manager.get_slugs()
+    engagement = engagement_manager.get_engagement(slugs[0])
+    print(engagement.get_slug())
+    print(engagement.get_source_urls())

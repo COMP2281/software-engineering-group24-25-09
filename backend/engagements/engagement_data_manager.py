f0bbded8b1977d99ef3d61997bc7341d60fa17ae
fn <64966099+f2reninj5@users.noreply.github.com>
2025-02-13 15:31:44 +0000

    Document EngagementDataManager methods

--- backend/engagements/engagement_data_manager.py
+++ backend/engagements/engagement_data_manager.py
@@ -5,15 +5,26 @@ from backend.engagements.engagement_data import EngagementData
 
 
 class EngagementDataManager:
-    def file_exists(self):
+    def file_exists(self) -> bool:
+        """
+        Check if data file exists.
+        :return: True if file exists, False otherwise.
+        """
         return os.path.isfile(self.file_path)
 
-    def load_engagements(self):
+    def load_engagements(self) -> None:
+        """
+        Load engagements from file if it exists.
+        """
         if self.file_exists():
             file = open(self.file_path, "rb")
             self.data = pickle.load(file)
 
-    def __init__(self, data_path: str):
+    def __init__(self, data_path: str) -> None:
+        """
+        Instantiate PageManager and load engagements.
+        :param data_path: Path to data directory.
+        """
         self.file_path = os.path.join(data_path, "engagements.pickle")
         self.page_manager = PageManager(data_path)
         self.data = {}

16af7128f673c10701fed9950aff2efcbaceb138
BatJeb13 <samdavey56@gmail.com>
2025-02-13 15:04:04 +0000

    Finished Slide maker and Created slide merger

--- backend/powerpoint/powerpoint_test.py
+++ backend/powerpoint/powerpoint_test.py
@@ -14,7 +14,7 @@
 # 4. Export full powerpoint
 
 from pptx import Presentation
-from pptx.util import Inches
+import win32com.client
 import os
 import random
 TEMPLATES = os.getenv("TEMPLATES", "templates") # Gets the path to the files
@@ -37,7 +37,7 @@ def create_slide(export_name, title_name, text, content):
         shape_type = str(shape_format.type).split(' (')[0]
         shape_index = shape.placeholder_format.idx
         placeholder = slide.placeholders[shape_index]
-
+        print(shape_type)
         # Change title 
         if shape_type == "TITLE":
             print("TITLE FOUND")
@@ -45,25 +45,40 @@ def create_slide(export_name, title_name, text, content):
 
 
         # Change text
-        if shape_type == "BODY":
+        elif shape_type == "BODY":
             print("TEXT BOX FOUND")
             placeholder.text = text     
 
         # Change content
-        if shape_type == "OBJECT":
+        elif shape_type == "PICTURE":
             print("CONTENT BOX FOUND")
-            
+            placeholder.insert_picture(content)
 
-
+    
     # Save the changed slide
     os.chdir("created_slides")
-    prs.save('new-file-name.pptx')
+    prs.save(export_name)
     os.chdir("..")
 
 
 # Group the slides together
-def export_powerpoint():
-    pass
+def merge_presentations(presentations, path):
+  os.chdir("created_slides")
+  ppt_instance = win32com.client.Dispatch('PowerPoint.Application')
+  prs = ppt_instance.Presentations.open(os.path.abspath(presentations[0]), True, False, False)
+
+  for i in range(1, len(presentations)):
+      prs.Slides.InsertFromFile(os.path.abspath(presentations[i]), prs.Slides.Count)
+
+  prs.SaveAs(os.path.abspath(path))
+  prs.Close()
+
+
+        
 
 # PowerPoint creation
-create_slide("test.pptx", "Test", "Content would be here Why bullet points", "test.png")
+create_slide("test3.pptx", "Test", "Content would be here Why bullet points", "test.png")
+
+
+merge_presentations(["test2.pptx", "test.pptx", "test3.pptx"],"FINAL.pptx")
+

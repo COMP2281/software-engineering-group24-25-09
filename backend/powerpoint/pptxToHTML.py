0319a3b2b5bc7a58ebdb9240bff5627a2196ba61
Rosie Hendriksen <rosiehendriksen@gmail.com>
2025-02-13 11:33:30 +0000

    Explore other pptx to html libraries

--- backend/powerpoint/pptxToHTML.py
+++ backend/powerpoint/pptxToHTML.py
@@ -6,13 +6,20 @@
 # Instantiate a Presentation object that represents a presentation file
 import aspose.slides as slides
 
-#pres = slides.Presentation(r'backend\powerpoint\created_slides\new-file-name.pptx')
+def first_option():
+    #pres = slides.Presentation(r'backend\powerpoint\created_slides\new-file-name.pptx')
 
-pres = slides.Presentation(r'C:\Users\rosie\OneDrive\Documents\2nd Year\Software Engineering\Here is a Title.pptx')
+    pres = slides.Presentation(r'C:\Users\rosie\OneDrive\Documents\2nd Year\Software Engineering\Here is a Title.pptx')
 
-controller = slides.export.ResponsiveHtmlController()
-htmlOptions = slides.export.HtmlOptions()
-htmlOptions.html_formatter = slides.export.HtmlFormatter.create_custom_formatter(controller)
+    controller = slides.export.ResponsiveHtmlController()
+    htmlOptions = slides.export.HtmlOptions()
+    htmlOptions.html_formatter = slides.export.HtmlFormatter.create_custom_formatter(controller)
 
-# Saving the presentation to HTML
-pres.save("backend\powerpoint\created_slides\PresentationToHTML_out.html", slides.export.SaveFormat.HTML, htmlOptions)
+    # Saving the presentation to HTML
+    pres.save("backend\powerpoint\created_slides\PresentationToHTML_out.html", slides.export.SaveFormat.HTML, htmlOptions)
+
+def second_option():
+    # this relies of the nuget .net library PPTXToHTML version 1.0.5
+    # youtube video here: https://www.youtube.com/watch?v=WaKJAOTLCQw
+    # ignore, this is for c sharp . smh smh
+    return

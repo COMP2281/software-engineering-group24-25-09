28c51a4ae861dcdbf8b0c28f26da1a9442648357
Jackaed <ed.andy.jackson@gmail.com>
2025-02-15 16:24:47 +0000

    Migrate frontend to electron, cleanup, update package set

--- frontend/server.js
+++
@@ -1,7 +0,0 @@
-import connect from 'connect'
-import serveStatic from 'serve-static'
-
-// Loads up the server
-connect()
-    .use(serveStatic("./"))
-    .listen(8080, () => console.log('Server running on 8080...'))

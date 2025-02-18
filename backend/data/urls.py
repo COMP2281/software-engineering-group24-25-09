e55e305f0fe31e9114596053da00fe648c0a265d
fn <64966099+f2reninj5@users.noreply.github.com>
2025-02-18 18:44:50 +0000

    Remove old files, combine URLs.txt into urls.py, format prompts.py

--- backend/data/urls.py
+++ backend/data/urls.py
@@ -4,4 +4,16 @@ urls = [
     "https://www.ecs.soton.ac.uk/careers/companies/ibm",
     "https://www.southampton.ac.uk/news/2014/11/14-ibm-watson-supercomputer.page",
     "https://warwick.ac.uk/newsandevents/pressreleases/warwick_and_ibm/",
+] + [
+    "https://newsroom.ibm.com/2019-10-16-IBM-Boards-the-Mayflower-Autonomous-Ship-Project",
+    "https://www.computer.org/csdl/magazine/an/2024/03/10444020/1ULQWLnPM3e",
+    "https://uk.linkedin.com/in/nicola-hodson-0342782",
+    "https://research.ibm.com/blog/10-years-dublin-lab",
+    "https://skillsbuild.org/",
+    "https://www.linkedin.com/company/ibm",
+    "https://research.ibm.com/people",
+    "https://www.ibm.com/blogs/think/uk-en/degree-apprentices-working-for-the-tech-sector/",
+    "https://en.wikipedia.org/wiki/IBM",
+    "https://research.ibm.com/blog/a-cognitive-in-car-companion-to-help-us-enjoy-the-journey",
+    "https://www.ibm.com/blogs/think/uk-en/this-is-john-creating-collaborative-projects-with-universities/",
 ]
--- backend/search/prompts.py
+++ backend/search/prompts.py
@@ -1,4 +1,6 @@
-prompts = ['"IBM" interacts with "University" article UK Ireland',
-'"IBM" "university" project UK Ireland',
-'"ibm" job fair UK Ireland',
-'"ibm" "University" lecture UK Ireland']
+prompts = [
+    '"IBM" interacts with "University" article UK Ireland',
+    '"IBM" "university" project UK Ireland',
+    '"ibm" job fair UK Ireland',
+    '"ibm" "University" lecture UK Ireland',
+]

79ed3b2fe337114bca4f6d1ccdf88ec954404e1b
fn <64966099+f2reninj5@users.noreply.github.com>
2025-02-19 12:29:48 +0000

    Extract classes into separate files

---
+++ backend/engagements/llm/llm.py
@@ -0,0 +1,58 @@
+import re
+import subprocess
+import httpx
+import ollama
+
+from backend.engagements.llm.prompt import Prompt
+from backend.engagements.llm.prompt_builder import PromptBuilder
+from backend.engagements.pages import Page
+
+
+class LLM:
+    def ollama_running(self):
+        """
+        Check if Ollama is running.
+        :return: True if Ollama is running, otherwise False.
+        :rtype: bool
+        """
+        # try to run Ollama list command
+        try:
+            self.client.list()
+            return True
+        # if it can't connect, return False
+        except httpx.ConnectError:
+            return False
+
+    def _start_ollama(self):
+        subprocess.Popen(["ollama", "show", self.model_name]).wait()
+
+    def start_ollama(self):
+        if self.ollama_running():
+            print("Ollama is already running")
+            return
+        else:
+            self._start_ollama()
+
+    def __init__(self, url: str, model_name: str):
+        self.url = url
+        self.model_name = model_name
+        self.client = ollama.Client(host=self.url)
+        self.start_ollama()
+
+    def _generate(self, prompt: Prompt):
+        return self.client.generate(
+            model=self.model_name, system=prompt.instruction, prompt=prompt.prompt
+        ).response
+
+    def summarise(self, page: Page):
+        response = self._generate(PromptBuilder.summarise(page.get_markdown_content()))
+        sentences = re.split(r"\n+", response)
+        return sentences
+
+    def employees(self, page: Page):
+        response = self._generate(PromptBuilder.employees(page.get_markdown_content()))
+        employees = re.split(r"\n+", response)
+        return employees
+
+    def title(self, page: Page):
+        return self._generate(PromptBuilder.title(page.get_markdown_content()))
--- backend/engagements/llm/prompt.py
+++ backend/engagements/llm/prompt.py
@@ -2,38 +2,3 @@ class Prompt:
     def __init__(self, instruction: str, prompt: str) -> None:
         self.instruction = instruction
         self.prompt = prompt
-
-
-class PromptBuilder:
-    @staticmethod
-    def summarise(source: str) -> Prompt:  # explicit word count
-        return Prompt(
-            """You are finding how IBM have been involved in educational outreach.
-            You will receive the content from a web page. It may contain irrelevant information outside of the main article.
-            Please list a few sentences of maximum 15 words each summarising the article.
-            Write each on a new line.
-            Do not confirm this message or say anything else.
-            Avoid any formatting or indentation.""",
-            source,
-        )
-
-    @staticmethod
-    def employees(source: str) -> Prompt:
-        return Prompt(
-            """You are finding IBM employees involved in educational outreach.
-            You will receive the content from a web page. It may contain irrelevant information outside of the main article.
-            Please list the name of each IBM employee involved.
-            Write each on a new line.
-            Do not confirm this message or say anything else.
-            Avoid any formatting or indentation.""",
-            source,
-        )
-
-    @staticmethod
-    def title(source: str) -> Prompt:
-        return Prompt(
-            """You will receive the content from a web page. It may contain irrelevant information outside of the main article.
-            Please write a short title that describes the content.
-            Do not confirm this message or say anything else.""",
-            source,
-        )

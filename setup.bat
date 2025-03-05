0d315819bc0487ee9d6e540f7de002d79a0b0c66
fn <64966099+f2reninj5@users.noreply.github.com>
2025-03-05 16:48:46 +0000

    Fix setup script

--- setup.bat
+++ setup.bat
@@ -1,6 +1,10 @@
 %userprofile%\AppData\Local\Programs\Python\Python313\python.exe -m venv .venv
-.venv\Scripts\activate
+call .venv\Scripts\activate
 pip install -r requirements.txt
-cd frontend && npm install && cd ..
+pushd frontend
+call npm install
+popd
 copy example.env .env
 python config.py
+echo Start the application with start.bat
+pause

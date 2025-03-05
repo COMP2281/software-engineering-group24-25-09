f9b6da959e475419b7ba20d668bacf46f167f545
fn <64966099+f2reninj5@users.noreply.github.com>
2025-03-05 18:17:09 +0000

    Add more Python checks in setup script

--- setup.bat
+++ setup.bat
@@ -1,29 +1,83 @@
-:: install llama model
-ollama pull llama3.1:8b
+@echo off
+setlocal enabledelayedexpansion
 
-:: check for python 3.13
+:: Check for Python 3.13
+
 :: Set the default Python path
 set "PYTHONPATH=%userprofile%\AppData\Local\Programs\Python\Python313\python.exe"
 
 :: Check if the default Python executable exists
-if not exist "%PYTHONPATH%" (
-    echo Default Python not found at: %PYTHONPATH%
-    set /p "PYTHONPATH=Please enter the correct Python path: "
+if exist "%PYTHONPATH%" (
+    :: Extract version from python --version
+    for /f "tokens=2 delims= " %%a in ('"%PYTHONPATH%" --version 2^>^&1') do (
+        set "PYTHONVERSION=%%a"
+    )
+    :: Check if verison is correct
+    if "!PYTHONVERSION:~0,4!"=="3.13" (
+        goto :found
+    )
 )
 
-:: Use the Python path to run a command, for example, display the version
+:: Try every Python executable in PATH
+for /f "delims=" %%p in ('where python') do (
+    :: Extract version from python --version
+    for /f "tokens=2 delims= " %%a in ('"%%p" --version 2^>^&1') do (
+        set "PYTHONVERSION=%%a"
+    )
+    :: Check if verison is correct
+    if "!PYTHONVERSION:~0,4!"=="3.13" (
+        set "PYTHONPATH=%%p"
+        goto :found
+    )
+)
+
+echo Python 3.13 not found
+set /p "PYTHONPATH=Please enter the correct Python path: "
+
+:: Check if the Python executable exists
+if exist "%PYTHONPATH%" (
+    :: Extract version from python --version
+    for /f "tokens=2 delims= " %%a in ('"%PYTHONPATH%" --version 2^>^&1') do (
+        set "PYTHONVERSION=%%a"
+    )
+    :: Check if verison is correct
+    if "!PYTHONVERSION:~0,4!"=="3.13" (
+        goto :found
+    ) else (
+        echo %PYTHONPATH% has version !PYTHONVERSION!
+    )
+) else (
+    echo %PYTHONPATH% not found
+)
+
+:: Exit if Python not found
+pause
+exit /b 1
+
+:: Continue if Python found
+:found
 echo Running Python from: %PYTHONPATH%
 "%PYTHONPATH%" --version
 
-pause
-
-%userprofile%\AppData\Local\Programs\Python\Python313\python.exe -m venv .venv
+:: Create a virtual environment
+"%PYTHONPATH%" -m venv .venv
 call .venv\Scripts\activate
+
+:: Install Python dependencies
 pip install -r requirements.txt
+
+:: Install Llama model
+ollama pull llama3.1:8b
+
+:: Install Node.js dependenceis
 pushd frontend
 call npm install
 popd
+
+:: Generate config file
 copy example.env .env
 python config.py
+
+
 echo Start the application with start.bat
 pause

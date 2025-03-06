50a9551ce60b83adf29dca8f0e429bf55907e919
fn <64966099+f2reninj5@users.noreply.github.com>
2025-03-06 03:25:00 +0000

    Inherit from Saver in PageManager

--- backend/engagements/pages/page_manager.py
+++ backend/engagements/pages/page_manager.py
@@ -1,46 +1,31 @@
-import os.path
-import pickle
 from backend.engagements.pages.page import Page
+from backend.saver import Saver
 
 
-class PageManager:
-    def file_exists(self) -> bool:
-        return os.path.isfile(self.file_path)
-
-    def load_pages(self) -> None:
-        if self.file_exists():
-            file = open(self.file_path, "rb")
-            self.pages = pickle.load(file)
-
+class PageManager(Saver):
     def __init__(self, data_path: str) -> None:
-        self.file_path = os.path.join(data_path, "pages.pickle")
-        self.pages = {}
-        self.load_pages()
-
-    def save_pages(self) -> None:
-        file = open(self.file_path, "wb")
-        pickle.dump(self.pages, file)
+        super().__init__(data_path, "pages")
 
     def add_page(self, url: str) -> None:
-        if url in self.pages:
+        if url in self.data:
             return
-        self.pages[url] = Page(url)
-        self.save_pages()
+        self.data[url] = Page(url)
+        self.save_data()
 
     def remove_page(self, url: str) -> None:
-        del self.pages[url]
-        self.save_pages()
+        del self.data[url]
+        self.save_data()
 
     def update_page(self, url: str) -> None:
-        if url not in self.pages:
+        if url not in self.data:
             return
-        self.pages[url].update()
-        self.save_pages()
+        self.data[url].update()
+        self.save_data()
 
     def get_page(self, url: str) -> Page:
-        if url not in self.pages:
+        if url not in self.data:
             self.add_page(url)
-        return self.pages[url]
+        return self.data[url]
 
     def get_all_page_urls(self) -> set[str]:
-        return set(self.pages.keys())
+        return set(self.data.keys())

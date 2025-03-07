a1699371b544655476013f4f7ae165071b3b5556
fn <64966099+f2reninj5@users.noreply.github.com>
2025-03-07 02:24:20 +0000

    Match build_url parameters to urlunparse

--- backend/web.py
+++ backend/web.py
@@ -4,9 +4,9 @@ import requests
 USER_AGENT = "Durham_COMP2281_2425_9_EngagementTracker/1.0 (+https://github.com/COMP2281/software-engineering-group24-25-09) Requests/2.32.3"
 
 
-def build_url(scheme: str, netloc: str, url="", path="", query="", fragment=""):
+def build_url(scheme: str, netloc: str, path="", params="", query="", fragment=""):
     # https://stackoverflow.com/a/15799706
-    return str(urlunparse((scheme, netloc, url, path, query, fragment)))
+    return str(urlunparse((scheme, netloc, path, params, query, fragment)))
 
 
 def get(*args, **kwargs) -> requests.Response:

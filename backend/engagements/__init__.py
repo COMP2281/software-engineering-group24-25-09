a87a0d791103c045c5a373b630acec82bba6948f
fn <64966099+f2reninj5@users.noreply.github.com>
2025-03-18 01:45:48 +0000

    Add slideshow exports to engagement module exports

--- backend/engagements/__init__.py
+++ backend/engagements/__init__.py
@@ -2,3 +2,4 @@ from engagements.llm import LLM
 from engagements.pages import GetPageException
 from engagements.search import Search, URL
 from engagements.engagement_manager import EngagementManager, CannotCrawlException
+from engagements.slideshow import SlideshowBuilder, EngagementSlide
